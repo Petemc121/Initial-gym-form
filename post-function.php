fd701cbc23f8d0fe7a1dc8558bfcbf16ac8e2481
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-01-08 20:25:05 +0900

    New post creation functionality
    Added functions to create new post on form submission, along with interacting with a JSON file to insert international number codes into mysql.

---
+++ post-function.php
@@ -0,0 +1,423 @@
+
+
+<?php
+/* Template Name: Form1*/
+get_header();
+include get_template_directory() . '/dbconfig.php';
+
+$query = $db->query("SELECT * FROM _S9Q_countries ORDER BY name ASC");
+
+ $rowCount1 = mysqli_num_rows($query);
+
+
+
+ $filename = get_template_directory_uri()."/Assets/Javascript/callingcodes.json";
+ 
+ $data = file_get_contents($filename);
+ $array = json_decode($data, true);
+
+ foreach($array as $row) {
+      $SQL = $db->query("INSERT INTO _S9Q_callcodes (countryName, code, phoneCode) VALUES('".$row["countryName"]."','".$row["code"]."','".$row["phoneCode"]."')");
+ }
+
+
+
+ 
+
+ 
+
+
+ function my_theme_create_new_gym() {
+ if (isset($_POST['d73he3gehj4ge6yr']) || wp_verify_nonce($_POST['d73he3gehj4ge6yr'], 'create_gym_form_submit' ))
+ {
+
+  $gymName = sanitize_text_field($_POST['gymName']);
+  $address1 = sanitize_text_field($_POST['Address1']);
+  $country = sanitize_text_field($_POST['country']);
+  $state = sanitize_text_field($_POST['state']);
+  $city = sanitize_text_field($_POST['city']);
+  $email = sanitize_text_field($_POST['email']);
+  $phone = sanitize_text_field($_POST['phone']);
+  $postcode = sanitize_text_field($_POST['postcode']);
+  
+
+  if ($gymName == '') {
+    echo "<style type='text/css'>
+    #gymName{
+      border-color:#fca1a1 !important;
+      border-width:3px !important;    }</style>";
+
+      echo "<style>#fillFields {display:block !important}</style>";
+
+      return false;
+  }
+
+  if ($address1 == '') {
+    echo "<style type='text/css'>
+    #inaddress1{
+      border-color:#fca1a1 !important;
+      border-width:3px !important;    }</style>";
+
+      echo "<style>#fillFields {display:block !important}</style>";
+
+      return false;
+
+  }
+
+  if ($country == '0') {
+    echo "<style type='text/css'>
+    #country{
+      border-color:#fca1a1 !important;
+      border-width:3px !important;    }</style>";
+
+      echo "<style>#fillFields {display:block !important}</style>";
+      return false;
+
+  }
+
+  if ($state == '0' || $state == '') {
+    echo "<style type='text/css'>
+    #state{
+      border-color:#fca1a1 !important;
+      border-width:3px !important;    }</style>";
+
+      echo "<style>#fillFields {display:block !important}</style>";
+      return false;
+
+  }
+
+  if ($city == '0' || $city == '') {
+    echo "<style type='text/css'>
+    #city{
+      border-color:#fca1a1 !important;
+      border-width:3px !important;    }</style>";
+
+      echo "<style>#fillFields {display:block !important}</style>";
+      return false;
+
+  }
+
+  if ($postcode == '') {
+    echo "<style type='text/css'>
+    #postcode{
+      border-color:#fca1a1 !important;
+      border-width:3px !important;    }</style>";
+
+      echo "<style>#fillFields {display:block !important}</style>";
+      return false;
+
+  }
+
+  if ($email == '') {
+    echo "<style type='text/css'>
+    #emailin{
+      border-color:#fca1a1 !important;
+      border-width:3px !important;
+    }</style>";
+
+    echo "<style>#fillFields {display:block !important}</style>";
+    return false;
+
+  } else {
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+     echo "<style>#emailValidMsg {display:block !important}</style>";
+     return false;
+
+  }
+  }
+
+  if ($phone == '') {
+    echo "<style type='text/css'>
+    #phone{
+      border-color:#fca1a1 !important;
+      border-width:3px !important;
+    }</style>";
+
+    echo "<style>#fillFields {display:block !important}</style>";
+    return false;
+
+  }
+
+  return true;
+  
+  
+ }
+}
+
+if ($_POST) {
+ if(my_theme_create_new_gym()){
+   if(is_user_logged_in()) {
+     global $current_user;
+
+     wp_get_current_user();
+
+     $user_login = $current_user->user_login;
+     $user_email = $current_user->user_email;
+     $user_firstname = $current_user->user_firstname;
+     $user_lastname = $current_user->user_lastname;
+     $user_id = $current_user->ID;
+
+    $post_title = sanitize_text_field($_POST['gymName']);
+
+    $taxonomy = 'country_state_city';
+    $new_post = array(
+              'post_title' =>$post_title,
+              'post_author' =>$user_id,
+              'post_type' => 'Gyms',
+              'post_name' => $post_title,
+                    
+
+    );
+
+       
+    $query2 =  $db->query("SELECT `name` FROM _S9Q_countries WHERE id = ".$_POST['country']);
+
+    while($row = $query2->fetch_assoc()){$country = $row['name'];}
+
+    $query3 =  $db->query("SELECT `name` FROM _S9Q_state WHERE id = ".$_POST['state']);
+    
+    while($row2 = $query3->fetch_assoc()){$state = $row2['name'];}
+
+    $query4 =  $db->query("SELECT `name` FROM _S9Q_city WHERE id = ".$_POST['city']);
+   
+   while($row3 = $query4->fetch_assoc()){$city = $row3['name'];}
+
+   
+    
+
+   function set_post_term( $new_post, $country,$state, $city, $taxonomy ) {	
+      $term1 = term_exists( $country, $taxonomy );
+      $term2 = term_exists( $state, $taxonomy );
+      $term3 = term_exists( $city, $taxonomy );
+
+      // If the taxonomy doesn't exist, then we create it	
+      	if ( 0 === $term1 || null === $term1 ) {	
+        $term1 = wp_insert_term(
+                $country,
+                $taxonomy,
+                array(
+                  'slug' => strtolower( str_ireplace( ' ', '-', $country ))
+                  )
+              );	
+      }	
+
+      if ( 0 === $term2 || null === $term2 ) {	
+        $term2 = wp_insert_term(
+                $state,
+                $taxonomy,
+                array(
+                  'parent'=> $term1['term_id'],
+                  'slug' => strtolower( str_ireplace( ' ', '-', $state ))
+                  )
+              );	
+      }	
+
+      if ( 0 === $term3 || null === $term3 ) {	
+        $term3 = wp_insert_term(
+                $city,
+                $taxonomy,
+                array(
+                  'parent'=> $term2['term_id'],
+                  'slug' => strtolower( str_ireplace( ' ', '-', $city ))
+                  )
+              );	
+      }	
+
+
+      // Then we can set the taxonomy	
+      $pid = wp_insert_post($new_post);
+      wp_set_post_terms( $pid, $term1, $taxonomy, true );	
+      wp_set_post_terms( $pid, $term2, $taxonomy, true );	
+      wp_set_post_terms( $pid, $term3, $taxonomy, true );	
+
+    }
+
+    set_post_term($new_post, $country, $state, $city, $taxonomy );
+    
+
+    
+
+   }
+  
+
+
+ } 
+
+ 
+}
+ 
+
+?>
+
+<style>
+
+label {
+  color:white;
+  font-weight:bold;
+}
+
+body {
+  margin-top:100px;
+}
+
+
+</style>
+
+<body>
+
+
+
+        <h1 id="nameLoc" class="titles">Name and Location</h1>
+        <div class="center">
+        <div Id="emailValidMsg" class="alert alert-danger" role="alert">
+        Please use a valid email address
+        </div>
+        </div>
+
+        <div class="center">
+        <div Id="fillFields" class="alert alert-danger" role="alert">
+        Please fill required fields
+        </div>
+        </div>
+
+      <div class="container forms" id ="form1">
+      <form name="initgymform" method="post" enctype="multipart/form-data">
+      <?php wp_nonce_field( 'create_gym_form_submit', 'd73he3gehj4ge6yr' ); ?>
+      <div class="form-group">
+          <label for="name">Gym Name</label>
+          <input name="gymName" class="form-control" id="gymName" >
+        </div>
+        <label>Gym Address</label>
+
+        <div class="row">
+          <div class="col-sm-6">
+        <label for="inaddress1" style="float:left;">Address line 1 </label>
+        <input type="text" name="Address1" class="form-control" id="inaddress1" placeholder="Street and building">
+      </div>
+      <div style="float:left;" class="col-sm-6">
+        <label for="inaddress2" style="float:left;">Address Line 2 (optional)</label>
+        <input type="text" name="Address2" class="form-control" id="inaddress2" placeholder="Flat number, Suite, Floor, etc">
+          </div>
+</div>
+<div class="row">
+          <div class="col-sm-6">
+        <label for="country" style="float:left;">Country</label>
+        <select name ="country" id="country" class="form-control">
+        <option value = "0">Select a country</option>
+        <?php 
+        if($rowCount1 > 0) {
+            while($row = $query->fetch_assoc()) {
+              echo '<option value="'.$row['id'].'">'.$row['name'].'</option>';
+            }
+        } else {
+          echo '<option value="">Country not availible</option>';
+        }
+        ?>
+        </select>
+      </div>
+      <div style="float:left;" class="col-sm-6">
+        <label for="postcode" style="float:left;">Postcode/Zip code</label>
+        <input name="postcode" class="form-control" id="postcode" >
+          </div>
+</div>
+<div class="row">
+          <div class="col-sm-6">
+        <label for="state" style="float:left;">State/province</label>
+        <select name ="state" id="state" class="form-control">
+        <option value = "0">Select a state/province</option>
+      </select>
+      </div>
+      <div style="float:left;" class="col-sm-6">
+        <label for="city" style="float:left;">City</label>
+        <select name ="city" id="city" class="form-control">
+        <option value ="0">Select a city</option>
+        </select>
+          </div>
+</div>
+     
+    </div>
+
+    <h1 class = "titles">Contact Details</h1>
+
+    <div class="container forms"  id= "form2">
+    
+    <div class="form-group">
+          <label for="exampleInputEmail1">Website</label>
+          <input type="text" name="website" class="form-control" id="webin" >
+        </div>
+        <div class="form-group">
+          <label for="exampleInputEmail1">Facebook Group</label>
+          <input type="text" type="facebook" class="form-control" id="fbin" >
+        </div>
+          <div class="form-group">
+          <label for="exampleInputEmail1">Email address</label>
+                <input type="text" name="email" class="form-control" id="emailin" aria-describedby="emailHelp">
+                <small id="emailHelp" class="form-text text-muted"></small>
+            </div>
+          <div  class="form-group"></div>
+            <label for="number">Phone number</label>
+      <select>
+        <option>Country Code</option>
+        <?php
+        $data = file_get_contents("callingcodes.json")
+        ?>
+      </select>
+            <input id= "phone" type="text" name="phone" class="form-control" id="phonein" >
+          </div>
+
+
+    
+    </div>
+
+    <div id="btnsub1d">
+    <button id="btnsub1" type="submit" name="gymsub" class="btn">Submit</button>
+    </div>
+    </form>
+    
+
+    <script>
+
+
+var element1 = document.querySelector("#form1");
+  setTimeout(function() {
+    element1.style.transform = "translatex(1400px)";
+  }, 100);
+
+  var element2 = document.querySelector("#form2");
+  setTimeout(function() {
+    element2.style.transform = "translatex(1400px)";
+  }, 100);
+
+  document.getElementById("btnsub1").onmouseover = function() {
+
+document.getElementById("btnsub1").style.backgroundImage = "linear-gradient(#AD0E2C, black)";
+
+};
+
+document.getElementById("btnsub1").onmouseout = function() {
+
+document.getElementById("btnsub1").style.backgroundImage = "linear-gradient(#0A0A23, #071A4B)";
+
+};
+
+</script>
+
+
+
+</body>
+
+<div>
+
+<?php
+
+ 
+
+get_footer();
+
+?>
+
+</div>
+
+
+
+
