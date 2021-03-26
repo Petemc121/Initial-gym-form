b3835c122a4b52cd1c1a1c5f8693c52bc72b8f37
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-26 16:30:09 +0900

    Update dropdownAjax.js
    Attempted to fix a value issue when using data lists, still woking on it

--- dropdownAjax.js
+++ dropdownAjax.js
@@ -1,37 +1,53 @@
+window.onload = function () {
+  $("#country").on("change", function (e) {
+    var input = e.target,
+      list = input.getAttribute("list"),
+      options = document.querySelectorAll("#" + list + " option"),
+      hiddenInput = document.getElementById(
+        input.getAttribute("id") + "-hidden"
+      ),
+      label = input.value;
 
+    hiddenInput.value = label;
 
-window.onload = function() {
-    $('#country').on('change',function(){
-        var countryID = $(this).val();
-        if(countryID){
-            $.ajax({
-                type:'POST',
-                url: ajax_object.ajax_url + '?action=load_countries',
-                data:'country_id='+ countryID,
-                success:function(html){
-                    $('#state').html(html);
-                    $('#city').html('<option value="">Select state first</option>'); 
-                }
-            }); 
-        }else{
-            $('#state').html('<option value="">Select country first</option>');
-            $('#city').html('<option value="">Select state first</option>'); 
-        }
-    });
+    for (var i = 0; i < options.length; i++) {
+      var option = options[i];
 
-    $('#state').on('change',function(){
-        var stateID = $(this).val();
-        if(stateID){
-            $.ajax({
-                type:'POST',
-                url: ajax_object.ajax_url + '?action=load_countries',
-                data:'state_id='+stateID,
-                success:function(html){
-                    $('#city').html(html);
-                }
-            }); 
-        }else{
-            $('#city').html('<option value="">Select state first</option>'); 
-        }
-    });
-  }
+      if (option.innerText === label) {
+        hiddenInput.value = option.getAttribute("data-value");
+        break;
+      }
+    }
+    var countryID = $(this).val();
+    if (countryID) {
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url + "?action=load_countries",
+        data: "country_id=" + countryID,
+        success: function (html) {
+          $("#state").html(html);
+          $("#city").html('<option value="">Select state first</option>');
+        },
+      });
+    } else {
+      $("#state").html('<option value="">Select country first</option>');
+      $("#city").html('<option value="">Select state first</option>');
+    }
+  });
+
+  $("#state").on("change", function () {
+    var stateID = $(this).val();
+    if (stateID) {
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url + "?action=load_countries",
+        data: "state_id=" + stateID,
+        success: function (html) {
+          $("#city").html(html);
+        },
+      });
+    } else {
+      $("#city").html('<option value="">Select state first</option>');
+    }
+  });
+};
