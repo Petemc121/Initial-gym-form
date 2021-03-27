8e85231bac5058232963e33f7eb65fa07d094dc5
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-27 11:42:14 +0900

    Update dropdownAjax.js

--- dropdownAjax.js
+++ dropdownAjax.js
@@ -1,37 +1,39 @@
+window.onload = function () {
+  $("#country").on("change", function () {
+    var shownVal = $(this).val();
+    var countryID = document.querySelector(
+      "#countryList option[value='" + shownVal + "']"
+    ).dataset.value;
 
+    if (countryID) {
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url + "?action=load_countries",
+        data: "country_id=" + countryID,
+        success: function (html) {
+          $("#state").html(html);
+          $("#city").html('<option value="">Select state first</option>');
+        },
+      });
+    } else {
+      $("#state").html('<option value="">Select country first</option>');
+      $("#city").html('<option value="">Select state first</option>');
+    }
+  });
 
-window.onload = function() {
-    $('#country').on('change',function(){
-        var countryID = $(this).val();
-        if(countryID){
-            $.ajax({
-                type:'POST',
-                url: ajax_object.ajax_url + '?action=load_countries',
-                data:'country_id='+ countryID,
-                success:function(html){
-                    $('#state').html(html);
-                    $('#city').html('<option value="">Select state first</option>'); 
-                }
-            }); 
-        }else{
-            $('#state').html('<option value="">Select country first</option>');
-            $('#city').html('<option value="">Select state first</option>'); 
-        }
-    });
-
-    $('#state').on('change',function(){
-        var stateID = $(this).val();
-        if(stateID){
-            $.ajax({
-                type:'POST',
-                url: ajax_object.ajax_url + '?action=load_countries',
-                data:'state_id='+stateID,
-                success:function(html){
-                    $('#city').html(html);
-                }
-            }); 
-        }else{
-            $('#city').html('<option value="">Select state first</option>'); 
-        }
-    });
-  }
+  $("#state").on("change", function () {
+    var stateID = $(this).val();
+    if (stateID) {
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url + "?action=load_countries",
+        data: "state_id=" + stateID,
+        success: function (html) {
+          $("#city").html(html);
+        },
+      });
+    } else {
+      $("#city").html('<option value="">Select state first</option>');
+    }
+  });
+};
