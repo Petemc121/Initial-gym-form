2a87034bd0a1075597393f4658597d95b1a02973
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-27 11:26:39 +0900

    Update dropdownAjax.js

--- dropdownAjax.js
+++ dropdownAjax.js
@@ -1,38 +1,44 @@
 window.onload = function () {
-  $("#country").on("change", function (e) {
-    var input = e.target,
-      list = input.getAttribute("list"),
-      options = document.querySelectorAll("#" + list + " option"),
-      hiddenInput = document.getElementById(
-        input.getAttribute("id") + "-hidden"
-      ),
-      label = input.value;
+  $("#country").on("change", function () {
+    document
+      .querySelector("input[list]")
+      .addEventListener("input", function (e) {
+        var input = e.target,
+          list = input.getAttribute("list"),
+          options = document.querySelectorAll("#" + list + " option"),
+          hiddenInput = document.getElementById(
+            input.getAttribute("id") + "-hidden"
+          ),
+          label = input.value;
 
-    hiddenInput.value = label;
+        hiddenInput.value = label;
 
-    for (var i = 0; i < options.length; i++) {
-      var option = options[i];
+        alert(label);
 
-      if (option.innerText === label) {
-        hiddenInput.value = option.getAttribute("data-value");
-        break;
-      }
-    }
-    var countryID = $(this).val();
-    if (countryID) {
-      $.ajax({
-        type: "POST",
-        url: ajax_object.ajax_url + "?action=load_countries",
-        data: "country_id=" + countryID,
-        success: function (html) {
-          $("#state").html(html);
+        for (var i = 0; i < options.length; i++) {
+          var option = options[i];
+
+          if (option.innerText === label) {
+            hiddenInput.value = option.getAttribute("data-value");
+            break;
+          }
+        }
+        var countryID = hiddenInput.value;
+        if (countryID) {
+          $.ajax({
+            type: "POST",
+            url: ajax_object.ajax_url + "?action=load_countries",
+            data: "country_id=" + countryID,
+            success: function (html) {
+              $("#state").html(html);
+              $("#city").html('<option value="">Select state first</option>');
+            },
+          });
+        } else {
+          $("#state").html('<option value="">Select country first</option>');
           $("#city").html('<option value="">Select state first</option>');
-        },
+        }
       });
-    } else {
-      $("#state").html('<option value="">Select country first</option>');
-      $("#city").html('<option value="">Select state first</option>');
-    }
   });
 
   $("#state").on("change", function () {
