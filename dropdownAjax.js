2747204f0fd2a2045ece55e353e6eaeda80a2938
Peter McNulty <67034896+Petemc121@users.noreply.github.com>
2021-03-27 11:55:14 +0900

    Update dropdownAjax.js
    Adapted ajax call to handling datalists instead of select inputs

--- dropdownAjax.js
+++ dropdownAjax.js
@@ -1,55 +1,38 @@
 window.onload = function () {
   $("#country").on("change", function () {
-    document
-      .querySelector("input[list]")
-      .addEventListener("input", function (e) {
-        var input = e.target,
-          list = input.getAttribute("list"),
-          options = document.querySelectorAll("#" + list + " option"),
-          hiddenInput = document.getElementById(
-            input.getAttribute("id") + "-hidden"
-          ),
-          label = input.value;
+    var shownVal = $(this).val();
+    var countryID = document.querySelector(
+      "#countryList option[value='" + shownVal + "']"
+    ).dataset.value;
 
-        hiddenInput.value = label;
-
-        alert(label);
-
-        for (var i = 0; i < options.length; i++) {
-          var option = options[i];
-
-          if (option.innerText === label) {
-            hiddenInput.value = option.getAttribute("data-value");
-            break;
-          }
-        }
-        var countryID = hiddenInput.value;
-        if (countryID) {
-          $.ajax({
-            type: "POST",
-            url: ajax_object.ajax_url + "?action=load_countries",
-            data: "country_id=" + countryID,
-            success: function (html) {
-              $("#state").html(html);
-              $("#city").html('<option value="">Select state first</option>');
-            },
-          });
-        } else {
-          $("#state").html('<option value="">Select country first</option>');
+    if (countryID) {
+      $.ajax({
+        type: "POST",
+        url: ajax_object.ajax_url + "?action=load_countries",
+        data: "country_id=" + countryID,
+        success: function (html) {
+          $("#stateList").html(html);
           $("#city").html('<option value="">Select state first</option>');
-        }
+        },
       });
+    } else {
+      $("#state").html('<option value="">Select country first</option>');
+      $("#city").html('<option value="">Select state first</option>');
+    }
   });
 
   $("#state").on("change", function () {
-    var stateID = $(this).val();
+    var shownVal = $(this).val();
+    var stateID = document.querySelector(
+      "#stateList option[value='" + shownVal + "']"
+    ).dataset.value;
     if (stateID) {
       $.ajax({
         type: "POST",
         url: ajax_object.ajax_url + "?action=load_countries",
         data: "state_id=" + stateID,
         success: function (html) {
-          $("#city").html(html);
+          $("#cityList").html(html);
         },
       });
     } else {
